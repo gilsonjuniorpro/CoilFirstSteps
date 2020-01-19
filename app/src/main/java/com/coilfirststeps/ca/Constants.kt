2244f6ceea4a210d40ecfd6ed89929b05572f0bf
Gilson <gilsonjuniorpro@gmail.com>
2020-01-18 21:07:04 -0500

    Initial commit

---
+++ app/src/main/java/com/coilfirststeps/ca/Constants.kt
@@ -0,0 +1,5 @@
+package com.coilfirststeps.ca
+
+object Constants {
+    val URL_IMAGE = "https://library.kissclipart.com/20180902/poq/kissclipart-cartoon-iron-man-transparent-clipart-iron-man-draw-aa36194200572509.jpg"
+}
---
+++ app/src/main/java/com/coilfirststeps/ca/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.coilfirststeps.ca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import coil.api.load
+import coil.transform.BlurTransformation
+import coil.transform.CircleCropTransformation
+import coil.transform.GrayscaleTransformation
+import coil.transform.RoundedCornersTransformation
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        image1.load(Constants.URL_IMAGE) {
+            crossfade(true)
+            crossfade(500)
+        }
+
+        image2.load(Constants.URL_IMAGE) {
+            crossfade(true)
+            crossfade(500)
+            transformations(CircleCropTransformation())
+        }
+
+        image3.load(Constants.URL_IMAGE) {
+            crossfade(true)
+            crossfade(500)
+            transformations(RoundedCornersTransformation(25f))
+        }
+
+        image4.load(Constants.URL_IMAGE) {
+            crossfade(true)
+            crossfade(500)
+            transformations(BlurTransformation(applicationContext, 5f))
+        }
+    }
+}
